46ac0b86d96c1ab8b294f4e19617244e1e817dd4
handsomevictor <857730556@qq.com>
2024-06-18 16:49:12 +0200

    updates

--- sync_version_from_profile.py
+++ sync_version_from_profile.py
@@ -2,6 +2,7 @@ import os
 import re
 import time
 import random
+from tqdm import tqdm
 from playwright.sync_api import sync_playwright
 
 USER_AGENTS = [
@@ -11,22 +12,50 @@ USER_AGENTS = [
 ]
 
 
-def main(linkedin_newsletter_urls):
+# noinspection PyShadowingNames
+def main(profile_url):
+    """
+    First get to the profile, and get the links of all the newsletters, and then get the content of each newsletter
+    """
     with sync_playwright() as p:
-        # browser = p.chromium.launch(headless=False)
-        # use firefox
-        browser = p.firefox.launch(headless=True)
+        browser = p.firefox.launch(headless=False)
         context = browser.new_context()
 
-        # 设置随机用户代理
+        # Set a random user agent
         user_agent = random.choice(USER_AGENTS)
         context.set_extra_http_headers({"User-Agent": user_agent})
 
-        # Open a new page
         page = context.new_page()
+        page.goto(f'{profile_url}')
+
+        # class = share-article__content
+        contents_newsletter_selector = '.share-article__content .share-article__title-link'
+
+        # page.wait_for_selector(contents_newsletter_selector)
+        page.wait_for_timeout(random.randint(800, 2000))
+
+        linkedin_newsletter_elements = page.locator(contents_newsletter_selector)
+
+        count = linkedin_newsletter_elements.count()
+        if count == 0:
+            raise Exception("No newsletter links found. Website is blocked or changed, trying again in 10 seconds.")
+
+        final_linkedin_newsletter_urls = []
+        for i in range(count):
+            element = linkedin_newsletter_elements.nth(i)
+            # get href attribute
+            inner_html = element.get_attribute('href')
+
+            final_linkedin_newsletter_urls.append(inner_html)
+
+        print(final_linkedin_newsletter_urls)
+        print(len(final_linkedin_newsletter_urls))
+
+        page.wait_for_timeout(random.randint(800, 2000))
 
         # Navigate to a URL
-        for idx, linkedin_newsletter_url in enumerate(linkedin_newsletter_urls):
+        for idx, linkedin_newsletter_url in tqdm(enumerate(final_linkedin_newsletter_urls),
+                                                 total=len(final_linkedin_newsletter_urls)):
             page.goto(f'{linkedin_newsletter_url}')
 
             # sleep random time use page.wait_for_timeout(random.randint(1000, 3000))
@@ -43,7 +72,6 @@ def main(linkedin_newsletter_urls):
             title_selector = '.pulse-title'
             page.wait_for_selector(title_selector)
             title_content = page.locator(title_selector).inner_text()
-            print(title_content)
 
             page.wait_for_timeout(random.randint(800, 2000))
 
@@ -54,7 +82,6 @@ def main(linkedin_newsletter_urls):
             article_content_selector = '[data-test-id="article-content-blocks"]'
             page.wait_for_selector(article_content_selector)
             article_content = page.locator(article_content_selector).inner_text()
-            print(article_content)
 
             page.wait_for_timeout(random.randint(800, 2000))
 
@@ -63,8 +90,9 @@ def main(linkedin_newsletter_urls):
                 page.wait_for_timeout(random.randint(800, 2000))
 
             # save to text
-            with open(f'article_{idx}.txt', 'w') as f:
+            with open(os.path.join(os.getcwd(), 'database', f'article_{idx}.txt'), 'w') as f:
                 f.write(title_content)
+                f.write('\n')
                 f.write(article_content)
 
         input('Press any key to close the browser')
@@ -72,10 +100,17 @@ def main(linkedin_newsletter_urls):
 
 
 if __name__ == '__main__':
-    # linkedin_newsletter_urls = ['https://www.linkedin.com/pulse/144-ita%25C3%25BA-opens-bitcoin-trading-platform-all-users-aaron-stanley-mqmzf/?trackingId=LZKGzDSSTpeL880pvhTRmw%3D%3D',
-    #     'https://www.linkedin.com/pulse/143-crypto-regulations-come-early-2025-says-bc-aaron-stanley-8ag2f/?trackingId=vEwXZg0xRbeeS0QTalUFeQ%3D%3D',
-    #     'https://www.linkedin.com/pulse/143-crypto-regulations-come-early-2025-says-bc-aaron-stanley-8ag2f/?trackingId=vEwXZg0xRbeeS0QTalUFeQ%3D%3D',
-    #     'https://www.linkedin.com/pulse/latam-crypto-report-14-bancolombia-launches-wenia-exchange-stanley-nce0e/?trackingId=y6uTU8F%2FR5ytwlzboqUNfA%3D%3D']
-    # main(linkedin_newsletter_urls)
+    profile_url = 'https://www.linkedin.com/newsletters/brazil-crypto-report-6921453107637862400/'
 
-    ...
+    try_count = 0
+    while try_count < 3:
+        try:
+            main(profile_url)
+            break
+        except Exception as e:
+            print(e)
+            print("An error occurred. Very possibly linkedin has detected the bot. Trying again in 10 seconds.")
+            time.sleep(10)
+
+
+
